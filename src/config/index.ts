9e2935b86b1bfebc55dfbe66e129c498d332bc62
Aridan Pantoja <aridanpantoja@gmail.com>
2024-12-08 12:51:41 -0300

    feat(config): add siteConfig for general information

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: 'Studio Layse Félix',
+  url: 'https://studiolaysefelix.com.br',
+  ogImage: 'https://studiolaysefelix.com.br/og.jpg',
+  description:
+    'Acreditamos que a verdadeira saúde e bem-estar vêm da harmonia entre o corpo e a mente.',
+  links: {
+    instagram: 'https://www.instagram.com/clinicalaysefelix/',
+    whatsapp:
+      'https://api.whatsapp.com/message/5LRJLJG4XV72H1?autoload=1&app_absent=0',
+  },
+}
+
+export type SiteConfig = typeof siteConfig
