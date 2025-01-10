e5d311957922f77dfbd89288497ab09df996ec57
Aridan Pantoja <aridanpantoja@gmail.com>
2025-01-10 17:28:29 -0300

    styles: add z-50 index to whatsapp button

--- src/components/whatsapp-button.tsx
+++ src/components/whatsapp-button.tsx
@@ -12,7 +12,7 @@ export function WhatsappButton() {
       animate="popOnScreen"
       href={siteConfig.links.whatsapp}
       target="_blank"
-      className="fixed bottom-4 right-4 flex h-12 w-12 items-center justify-center rounded-full bg-[#25d366] text-white shadow-md hover:bg-[#32e575] md:h-14 md:w-14 [&_svg]:size-6"
+      className="fixed bottom-4 right-4 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-[#25d366] text-white shadow-md hover:bg-[#32e575] md:h-14 md:w-14 [&_svg]:size-6"
     >
       <WhatsappIcon />
     </MotionLink>
